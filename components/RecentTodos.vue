c5a12af68974a3291e16dc85b84d1a053d76c731
정상우 <zabefofoon@gmail.com>
2024-01-15 22:15:33 +0900

    recentTodos 추가

--- components/RecentTodos.vue
+++ components/RecentTodos.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-col gap-2 | border | p-2 lg:p-3">
     <div class="font-bold">Recent</div>
-    <div v-if="todoStore.todos?.length" class="grid grid-cols-2 lg:grid-cols-4 gap-2">
+    <div
+      v-if="todoStore.todos?.length"
+      class="grid grid-cols-2 lg:grid-cols-4 gap-2">
       <TodoThumbnail
-        v-for="todo in todoStore.todos?.slice(0, 4)"
+        v-for="todo in recentTodos"
         :key="todo.id"
         :todo="todo"
         hide-delete
@@ -14,9 +16,15 @@
 </template>
 
 <script setup lang="ts">
-import { useTodoStore } from '~/store/todo.store';
+import { useTodoStore } from '~/store/todo.store'
 
 const todoStore = useTodoStore()
+
+const recentTodos = computed(() => {
+  return todoStore.todos
+    ?.sort((a, b) => Number(b.modified) - Number(a.modified))
+    .slice(0, 4)
+})
 </script>
 
 <style></style>

32eb69714aff68767151a9af2896ce413390c880
정상우 <zabefofoon@gmail.com>
2024-05-18 02:53:24 +0900

    로컬스토리지 에러시, alert 노출

--- api/storage.api.ts
+++ api/storage.api.ts
@@ -1,10 +1,23 @@
 import store from 'store2'
 
-export const getLocalStorage = (key: string) => store.local.get(key)
+export const getLocalStorage = (key: string) => {
+  try {
+    return store.local.get(key)
+  } catch (e) {
+    alert(useI18n().t('BrowserNotice'))
+    return undefined
+  }
+}
 
-export const setLocalStorage = (key: string, data: string) => store.local.set(key, data)
+export const setLocalStorage = (key: string, data: string) => {
+  try {
+    store.local.set(key, data)
+  } catch {
+    alert(useI18n().t('BrowserNotice'))
+  }
+}
 
 export default {
-    getLocalStorage,
-    setLocalStorage
+  getLocalStorage,
+  setLocalStorage,
 }

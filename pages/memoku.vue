1c67bf0992bda57be72590611560853451b378a1
정상우 <zabefofoon@gmail.com>
2024-05-22 22:33:32 +0900

    RDFa, canonical, ld+json 추가

--- pages/memoku.vue
+++ pages/memoku.vue
@@ -1,13 +1,20 @@
 <template>
   <header
-    class="flex items-center gap-1.5 | px-4 lg:px-8 py-2 | border-b | sticky top-0 left-0 z-10 | bg-white">
-    <NuxtLink class="block | w-full | font-bold" to="/" area-label="Home">
+    class="flex items-center gap-1.5 | px-4 lg:px-8 py-2 | border-b | sticky top-0 left-0 z-10 | bg-white"
+    vocab="http://schema.org/"
+    typeof="WebPage">
+    <NuxtLink
+      class="block | w-full | font-bold"
+      to="/"
+      area-label="Home"
+      property="url">
       <div class="flex items-center justify-center gap-1 | w-fit">
         <img
           class="w-[24px] aspect-square"
           src="~/assets/images/logo.svg"
-          alt="MEMOKU" />
-        <span>MEMOKU</span>
+          alt="MEMOKU"
+          property="logo" />
+        <span property="name">MEMOKU</span>
       </div>
     </NuxtLink>
     <div>
@@ -26,10 +33,13 @@
   </header>
   <div class="flex flex-col justify-center">
     <section
-      class="relative | main-section | flex flex-col items-center justify-center | aspect-[1/1] lg:aspect-[16/9] max-h-[88vh]">
+      class="relative | main-section | flex flex-col items-center justify-center | aspect-[1/1] lg:aspect-[16/9] max-h-[88vh]"
+      property="mainContentOfPage">
       <img
         class="absolute z-0 bottom-0 lg:bottom-[unset] | pointer-events-none | w-[92%]"
-        src="~/assets/images/main.png" />
+        src="~/assets/images/main.png"
+        alt="Welcome to MEMOKU!"
+        property="image" />
       <div class="relative | bg-white">
         <h2
           class="flex gap-1.5 | text-sm | absolute top-0 left-0 -translate-y-[100%]">
@@ -68,79 +78,114 @@
       class="flex flex-col items-center justify-center gap-10 | px-4 py-24 | bg-slate-800">
       <ul class="grid grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8 | lg:w-[80%]">
         <li
-          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6">
+          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6"
+          typeof="Service">
           <div
             class="text-center lg:text-[24px] | flex items-center justify-center gap-2">
             <i class="icon icon-post flex-shrink-0 | text-white"></i>
-            <span class="text-white">{{ $t('EasyMemoDescription') }}</span>
+            <span class="text-white" property="name">{{
+              $t('EasyMemoDescription')
+            }}</span>
           </div>
-          <div class="text-center lg:text-[18px] text-white">
+          <div
+            class="text-center lg:text-[18px] text-white"
+            property="description">
             {{ $t('EasyMemoDescriptionGuide') }}
           </div>
         </li>
         <li
-          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6">
+          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6"
+          typeof="Service">
           <div
             class="text-center lg:text-[24px] | flex items-center justify-center gap-2">
             <i class="icon icon-notification flex-shrink-0 | text-white"></i>
-            <span class="text-white">{{ $t('NotificationDescription') }}</span>
+            <span class="text-white" property="name">{{
+              $t('NotificationDescription')
+            }}</span>
           </div>
-          <div class="text-center lg:text-[18px] text-white">
+          <div
+            class="text-center lg:text-[18px] text-white"
+            property="description">
             {{ $t('NotificationDescriptionDescription') }}
           </div>
         </li>
         <li
-          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6">
+          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6"
+          typeof="Service">
           <div
             class="text-center lg:text-[24px] | flex items-center justify-center gap-2">
             <i class="icon icon-tag flex-shrink-0 | text-white"></i>
-            <span class="text-white">{{ $t('TagDescription') }}</span>
+            <span class="text-white" property="name">{{
+              $t('TagDescription')
+            }}</span>
           </div>
-          <div class="text-center lg:text-[18px] text-white">
+          <div
+            class="text-center lg:text-[18px] text-white"
+            property="description">
             {{ $t('TagDescriptionGuide') }}
           </div>
         </li>
         <li
-          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6">
+          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6"
+          typeof="Service">
           <div
             class="text-center lg:text-[24px] | flex items-center justify-center gap-2">
             <i class="icon icon-date flex-shrink-0 | text-white"></i>
-            <span class="text-white">{{ $t('DateDescription') }}</span>
+            <span class="text-white" property="name">{{
+              $t('DateDescription')
+            }}</span>
           </div>
-          <div class="text-center lg:text-[18px] text-white">
+          <div
+            class="text-center lg:text-[18px] text-white"
+            property="description">
             {{ $t('DateDescriptionGuide') }}
           </div>
         </li>
         <li
-          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6">
+          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6"
+          typeof="Service">
           <div
             class="text-center lg:text-[24px] | flex items-center justify-center gap-2">
             <i class="icon icon-theme flex-shrink-0 | text-white"></i>
-            <span class="text-white">{{ $t('ThemeDescription') }}</span>
+            <span class="text-white" property="name">{{
+              $t('ThemeDescription')
+            }}</span>
           </div>
-          <div class="text-center lg:text-[18px] text-white">
+          <div
+            class="text-center lg:text-[18px] text-white"
+            property="description">
             {{ $t('ThemeDescriptionGuide') }}
           </div>
         </li>
         <li
-          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6">
+          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6"
+          typeof="Service">
           <div
             class="text-center lg:text-[24px] | flex items-center justify-center gap-2">
             <i class="icon icon-chart flex-shrink-0 | text-white"></i>
-            <span class="text-white">{{ $t('AnalisysDescription') }}</span>
+            <span class="text-white" property="name">{{
+              $t('AnalisysDescription')
+            }}</span>
           </div>
-          <div class="text-center lg:text-[18px] text-white">
+          <div
+            class="text-center lg:text-[18px] text-white"
+            property="description">
             {{ $t('AnalisysDescriptionGuide') }}
           </div>
         </li>
         <li
-          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6">
+          class="flex flex-col gap-4 lg:gap-8 | w-full | border border-slate-500 rounded-lg | p-4 lg:py-10 lg:px-6"
+          typeof="Service">
           <div
             class="text-center lg:text-[24px] | flex items-center justify-center gap-2">
             <i class="icon icon-offline flex-shrink-0 | text-white"></i>
-            <span class="text-white">{{ $t('OfflineDescription') }}</span>
+            <span class="text-white" property="name">{{
+              $t('OfflineDescription')
+            }}</span>
           </div>
-          <div class="text-center lg:text-[18px] text-white">
+          <div
+            class="text-center lg:text-[18px] text-white"
+            property="description">
             {{ $t('OfflineDescriptionGuide') }}
           </div>
         </li>
@@ -149,7 +194,11 @@
     <section
       class="flex items-center justify-center flex-col lg:flex-row gap-12 lg:gap-0 | py-20">
       <div>
-        <img src="~/assets/images/add_home.png" class="w-full" />
+        <img
+          src="~/assets/images/add_home.png"
+          class="w-full"
+          alt="Responsive MEMOKU"
+          property="image" />
       </div>
       <div class="flex flex-col gap-3 | w-full | px-8">
         <h3 class="text-2xl lg:text-3xl leading-normal | lg:w-1/2">
@@ -166,13 +215,18 @@
       </div>
     </section>
   </div>
-  <footer
-    class="bg-slate-950 | px-4 lg:px-8 py-6 | flex justify-between gap-4">
+  <footer class="bg-slate-950 | px-4 lg:px-8 py-6 | flex justify-between gap-4">
     <h2 class="flex items-center gap-2">
-      <span class="text-white">MEMOKU</span>
-      <span class="hidden lg:inline ml-auto | text-xs text-white">#{{ $t('TodoList') }}</span>
-      <span class="hidden lg:inline text-xs text-white">#{{ $t('Reminder') }} </span>
-      <span class="hidden lg:inline text-xs text-white">#{{ $t('TaskManager') }} </span>
+      <span class="text-white" property="name">MEMOKU</span>
+      <span class="hidden lg:inline ml-auto | text-xs text-white"
+        >#{{ $t('TodoList') }}</span
+      >
+      <span class="hidden lg:inline text-xs text-white"
+        >#{{ $t('Reminder') }}
+      </span>
+      <span class="hidden lg:inline text-xs text-white"
+        >#{{ $t('TaskManager') }}
+      </span>
     </h2>
     <div class="flex items-center gap-3">
       <NuxtLink
@@ -201,4 +255,95 @@ const changeLanguage = (event: Event) => {
   const value = <Language>(<HTMLSelectElement>event.target).value
   storageStore.setLanguage(value)
 }
+
+useHead({
+  link: [
+    {
+      rel: 'canonical',
+      href: 'https://memoku.netlify.app/memoku'
+    }
+  ],
+  script: [
+    {
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify({
+        '@context': 'http://schema.org',
+        '@type': 'WebPage',
+        name: 'MEMOKU',
+        url: 'https://memoku.netlify.app/memoku',
+        description:
+          'MEMOKU is a task management web app that helps you manage tasks easily with features like easy memo, notifications, tags, date management, dark mode, task analysis, and offline mode.',
+        mainEntity: {
+          '@type': 'WebApplication',
+          name: 'MEMOKU',
+          operatingSystem: 'All',
+          applicationCategory: 'Productivity',
+          offers: {
+            '@type': 'Offer',
+            price: '0',
+            priceCurrency: 'USD',
+            category: 'Free',
+          },
+        },
+        about: {
+          '@type': 'Thing',
+          name: 'Task Management',
+          description:
+            'Manage tasks, set reminders, and organize your life with MEMOKU.',
+        },
+        creator: {
+          '@type': 'Person',
+          name: 'saaang',
+          url: 'https://www.threads.net/@sangwwooo/post/C7E92AAv_pz',
+        },
+        potentialAction: {
+          '@type': 'Action',
+          name: 'Get Started',
+          target: 'https://memoku.netlify.app/',
+        },
+        screenshot: '/screenshot-2.png',
+        featureList: [
+          {
+            '@type': 'WebApplicationFeature',
+            name: 'Easy Memo',
+            description: 'Can easily memo your tasks with a few clicks.',
+          },
+          {
+            '@type': 'WebApplicationFeature',
+            name: 'Notification',
+            description:
+              "Can set notifications so you don't miss important tasks.",
+          },
+          {
+            '@type': 'WebApplicationFeature',
+            name: 'Tag',
+            description: 'Can set tags to easily classify and manage tasks.',
+          },
+          {
+            '@type': 'WebApplicationFeature',
+            name: 'Date',
+            description: 'Can register tasks by date for planned management.',
+          },
+          {
+            '@type': 'WebApplicationFeature',
+            name: 'Theme',
+            description:
+              'Choose between Light Mode and Dark Mode according to your preference.',
+          },
+          {
+            '@type': 'WebApplicationFeature',
+            name: 'Analysis',
+            description: 'Provides statistics by simply analyzing your tasks.',
+          },
+          {
+            '@type': 'WebApplicationFeature',
+            name: 'Offline',
+            description:
+              "Catch up on your tasks even when you're offline with our app.",
+          },
+        ],
+      }),
+    },
+  ],
+})
 </script>

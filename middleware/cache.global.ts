0140cd5ecdc55ce14503beff688f3c4c0928f45f
zabefofoon <zabefofoon@gmail.com>
2024-10-22 23:32:03 +0900

    cache 방식 변경

--- middleware/cache.global.ts
+++ middleware/cache.global.ts
@@ -1,13 +1,16 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   if (process.client && window.caches) {
-    const cache = await caches.open('memoku-cache-13')
-    const keys = await cache.keys()
-    keys.forEach((key) => {
-      const path = key.url.replace(location.origin, '')
-      if (path !== '/' && path !== to.path) cache.delete(key)
-    })
+    caches.open('memoku-cache-13').then((cache) => {
+      cache.keys().then((keys) => {
+        keys.forEach((key) => {
+          const path = key.url.replace(location.origin, '')
+          if (path !== '/' && path !== to.path) cache.delete(key)
+        })
 
-    const response = await fetch('/', {cache: 'force-cache'})
-    cache.put(to.path, response)
+        fetch('/', { cache: 'force-cache' }).then((response) =>
+          cache.put(to.path, response)
+        )
+      })
+    })
   }
 })

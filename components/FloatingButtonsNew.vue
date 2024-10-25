cf44c89b793e9a3e020b9d03d429cbe9eb3a2e8b
zabefofoon <zabefofoon@gmail.com>
2024-10-26 00:08:07 +0900

    가이드 버튼 표시 로직 변경, 색상 변경

--- components/FloatingButtonsNew.vue
+++ components/FloatingButtonsNew.vue
@@ -2,28 +2,16 @@
   <NuxtLinkLocale
     :to="to"
     class="flex | bg-slate-800 rounded-full | text-white | p-2 | relative"
-    area-label="New todo">
+    area-label="New todo"
+    @click="guideStore.showAddGuide(true)">
     <i class="icon icon-add text-2xl text-white"></i>
     <ClientOnly>
       <div
-        v-if="!loading.todoLoading && guideStore.isShowAddGuide"
+        v-if="!loading.todoLoading && !guideStore.isShowAddGuide"
         class="add-guide | px-3 py-1.5 | absolute -left-1 top-0 -translate-x-full -translate-y-1/2 | text-center text-xs lg:text-sm whitespace-nowrap"
         :class="
-          storageStore.getThemeClass('bg-slate-200', 'bg-slate-800 text-white')
-        "
-        @click="guideStore.showAddGuide(false)">
-        <button
-          class="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 | rounded-full | flex"
-          :class="
-            storageStore.getThemeClass(
-              'bg-white | border border-slate-400',
-              'bg-slate-600'
-            )
-          ">
-          <i
-            class="icon icon-close text-xs"
-            :class="storageStore.getThemeClass('', 'text-white')"></i>
-        </button>
+          storageStore.getThemeClass('bg-slate-800', 'bg-slate-800 text-white')
+        ">
         <div
           class="absolute bottom-0 right-0 translate-x-1/2 translate-y-1/2 | w-2 h-2 | rounded-full | bg-red-500"></div>
         {{ i18n.t('AddGuide') }}
--- store/guide.store.ts
+++ store/guide.store.ts
@@ -4,10 +4,11 @@ import etcUtil from '~/utils/etc'
 export const useGuideStore = defineStore(
   'guide',
   () => {
-    const isShowAddGuide = ref(true)
+    false
+    const isShowAddGuide = ref(false)
     const showAddGuide = (value: boolean) => (isShowAddGuide.value = value)
 
-    const isShowAddHomeGuide = ref(true)
+    const isShowAddHomeGuide = ref(false)
     const showAddHomeGuide = (value: boolean) => {
       isShowAddHomeGuide.value = value
     }

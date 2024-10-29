accd6bb64a769c75e1443ad6f4d7b3079a5bd2a8
zabefofoon <zabefofoon@gmail.com>
2024-10-30 02:51:46 +0900

    blockIpList 추가

--- netlify/edge-functions/block-bots.js
+++ netlify/edge-functions/block-bots.js
@@ -1,21 +1,18 @@
 export default async (request) => {
-  const blockedIPs = ['123.456.789.101', '234.567.890.123']
   const clientIP = request.headers.get('x-forwarded-for')
   const referer = request.headers.get('referer')
   const userAgent = request.headers.get('user-agent')
-  console.log(
-    `Blocked IP: ${clientIP}, ${request.headers.get('referer')}, ${userAgent}`
-  )
+  console.log(`${clientIP}, ${request.headers.get('referer')}, ${userAgent}`)
 
-  const blackIPList = ['3.81.228.87']
+  const blockIPList = ['3.81.228.87', '193.70.113.250']
 
-  const blackRefererList = ['--memoku.netlify.app/']
+  const blockRefererList = ['--memoku.netlify.app/']
 
   const blackAgentList = ['Amazonbot']
 
   if (
-    blackIPList.includes(clientIP) ||
-    blackRefererList.includes(referer) ||
+    blockIPList.includes(clientIP) ||
+    blockRefererList.includes(referer) ||
     userAgent.includes(blackAgentList)
   ) {
     return new Response('Access denied', { status: 403 })
